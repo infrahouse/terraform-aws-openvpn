d62ab45a69a2f07c54a7879820d58b62748747c1
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-07-03 10:48:41 -0700

    Fix certificate CN
    * It was a fixed string. Now user's email.
    * Specify Python version for portal 3.12.
    * Add setup instructions

--- portal/publish.sh
+++ portal/publish.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+set -eux
+
 export AWS_DEFAULT_REGION=us-east-2
 
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 303467602807.dkr.ecr.us-east-2.amazonaws.com
+AWS_PROFILE="infrahouse-admin-cicd"
+
+aws --profile $AWS_PROFILE sts get-caller-identity || aws --profile infrahouse-admin-cicd sso login
+
+aws --profile $AWS_PROFILE ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 303467602807.dkr.ecr.us-east-2.amazonaws.com
 docker build -t portal .
 docker tag portal:latest 303467602807.dkr.ecr.us-east-2.amazonaws.com/portal:latest
 docker push 303467602807.dkr.ecr.us-east-2.amazonaws.com/portal:latest
 
 
-aws ecs update-service --cluster openvpn-portal --service openvpn-portal --force-new-deployment
-aws ecs wait services-stable --cluster openvpn-portal --services openvpn-portal
+aws --profile $AWS_PROFILE ecs update-service --cluster openvpn-portal --service openvpn-portal --force-new-deployment > /dev/null
+echo "Restarting the portal service. Please wait..."
+aws --profile $AWS_PROFILE ecs wait services-stable --cluster openvpn-portal --services openvpn-portal
+echo "done"
