974af9fcbaccf55cf4425a6977164ae196be2ffa
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-12-06 17:48:18 -0800

    test: add portal URL logging and output
    Add portal URL to test outputs and log it after deployment for easier
    access during testing.
    
    **Changes:**
    - test_data/openvpn/outputs.tf: Add portal_url output
    - tests/test_module.py: Log portal URL after service restart
    - tests/test_module.py: Minor formatting cleanup (whitespace alignment)
    
    **Benefits:**
    - Developers can easily access the portal URL from test output
    - No need to manually construct the URL from zone name
    - Improved developer experience during testing
    
    Portal URL is now logged after successful deployment:
      Portal URL: https://openvpn-portal.ci-cd.infrahouse.com

--- test_data/openvpn/outputs.tf
+++ test_data/openvpn/outputs.tf
@@ -5,3 +5,8 @@ output "google_client_secret" {
 output "account_id" {
   value = data.aws_caller_identity.this.account_id
 }
+
+output "portal_url" {
+  description = "URL of the OpenVPN portal web interface"
+  value       = module.openvpn.portal_url
+}
